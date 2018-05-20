3c8011a878b6625ddc0d667a7d3f9a69d057083c
nikiedev <nikiedev@mail.ru>
2018-05-20 19:31:33 +0300

    db config encoding changed to utf8.

--- config/db.php
+++ config/db.php
@@ -5,7 +5,7 @@ return [
     'dsn' => 'mysql:host=localhost;dbname=yii2basic-blog',
     'username' => 'root',
     'password' => '',
-    'charset' => 'windows1251',
+    'charset' => 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
